fc511559016579c9df929d1a07e0431d3f680c65
William <teststuffreally@gmail.com>
2019-09-24 15:03:06 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,7 @@
 # the answers. You should ONLY check those answers if you get totally stuck. The important
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
+<<<<<<< HEAD
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
@@ -11,6 +12,9 @@ end
 def introduction_with_language_optional(name,language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+def introduction 
+>>>>>>> e3eb43f8e84b6d1feb7403195b01d30c5ae3cbea
 
 
 
